d3042f6419954b4cd9a37b898b8463e52db74f0d
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-09-28 16:55:49 +0100

    Add hover to share

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const bubble = document.querySelector("blockquote");
 const share = document.getElementById("share");
+const body = document.querySelector(".card");
 
 share.addEventListener("click", function(event){
+    
     bubble.classList.add("clicked")
+    
 })
+
+
+
 
 bubble.addEventListener("click", function(event) {
     bubble.classList.remove("clicked")
